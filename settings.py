1d8c797a5ff9a0279736ed6e9cae4920c376122b
chendong <123309778@qq.com>
2019-11-18 13:14:07 +0800

    代理池工具模块: 日志模块与随机User-Agent请求头

--- settings.py
+++ settings.py
@@ -4,7 +4,16 @@
 # @Date : 2019-11-18 10:27
 
 
+import logging
+
+
 # 在配置文件: settings.py 中定义MAX_SCORE = 50,表示代理IP的默认最高分值
 MAX_SCORE = 50
+
+# 日志的配置信息
+LOG_LEVEL = logging.DEBUG  # INFO默认等级
+LOG_MFT = '%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s: %(message)s'  # 默认日志格式
+LOG_DATEMFT = '%Y-%m-%d %H:%M:%S'  # 默认时间格式
+LOG_FILENAME = 'log.log'  # 默认日志文件名称
 
 
